dc2b6af4386a00431e020d2204b417266bd39fc5
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-12-03 11:42:55 -0300

    first commit

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export function QuestionTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRmaningTime] = useState(timeout);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRmaningTime((prevRemaningTime) => prevRemaningTime - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
+}
